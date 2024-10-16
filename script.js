fb4af9f513d2549446c76e30bad11ddfa0be8240
barbaralfonseca <barbaradeliraf@gmail.com>
2024-10-16 16:31:02 -0300

    jogo java 02

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ let posY = 50;
 const moveSpeed = 50;
 const originalBackgroundColor = "black"
 
+document.addEventListener('keydown', (event) => {
+    switch (event.code) {
+        case 'ArrowUp':
+            if (posY > 0)
+                posY = posY - moveSpeed;
+            break;
+        case 'Arrowdown':
+            if (posY < window.innerHeight - circle.offsetHeight)
+                posY = posY + moveSpeed;
+            break;
+        case 'ArrowLeft':
+            if (posX > 0)
+                posX = posX - moveSpeed;
+            break
 
+    }
+})
