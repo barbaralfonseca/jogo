732fcaa135ff400202fe12ee9ff3069c3320e225
barbaralfonseca <barbaradeliraf@gmail.com>
2024-10-16 15:52:11 -0300

    jogo java

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const circle = document.getElementById("circle");
 const gameArea = document.getElementById("gameArea");
+
 let posX = 50;
 let posY = 50;
+
+const moveSpeed = 50;
+const originalBackgroundColor = "black"
+
+
